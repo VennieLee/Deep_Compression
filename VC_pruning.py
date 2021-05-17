aa170e96145e3b602ef6595756cee58493d5fc64
leeyeojin3 <81789158+leeyeojin3@users.noreply.github.com>
2021-05-17 17:25:50 +0900

    Update VC_pruning.py

--- VC_pruning.py
+++ VC_pruning.py
@@ -20,13 +20,13 @@ os.makedirs('saves', exist_ok=True)
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 pruning from deep compression paper')
-parser.add_argument('--batch-size', type=int, default=100, metavar='N',
+parser.add_argument('--batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for training (default: 50)')
-parser.add_argument('--test-batch-size', type=int, default=100, metavar='N',
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=5, metavar='N',
+parser.add_argument('--epochs', type=int, default=1, metavar='N',
                     help='number of epochs to train (default: 100)')
-parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
